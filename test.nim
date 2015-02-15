ea2b60ef545d7db4dc6a90a166e1c192fc37e020
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2015-02-15 16:08:25 -0700

    proc-ize the test

--- test.nim
+++ test.nim
@@ -1,21 +1,22 @@
 import objects, querying
 
-var world = createWorld()
-var root = blankObject()
-world.add(root)
-root.setPropR("name", "root")
-root.setPropR("aliases", @[])
-root.setPropR("location", root)
-root.setPropR("rootprop", "yes")
+proc testInheritance =
+  var world = createWorld()
+  var root = blankObject()
+  world.add(root)
+  root.setPropR("name", "root")
+  root.setPropR("aliases", @[])
+  root.setPropR("location", root)
+  root.setPropR("rootprop", "yes")
 
-var child = root.createChild()
-child.setPropR("name", "child")
+  var child = root.createChild()
+  child.setPropR("name", "child")
 
-echo child.getPropVal("rootprop").repr
+  var evenMoreChild = child.createChild()
+  evenMoreChild.setPropR("rootprop", "no")
 
-var evenMoreChild = child.createChild()
-evenMoreChild.setPropR("rootprop", "no")
+  child.changeParent(evenMoreChild)
 
-child.changeParent(evenMoreChild)
+  assert(child.getPropVal("rootprop").strVal == "no")
 
-echo child.getPropVal("rootprop").repr
+testInheritance()
