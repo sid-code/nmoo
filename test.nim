c2a5d14d8c62023a58fef1135c2a424d6c4f4e52
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2015-02-17 06:12:26 -0700

    Test cases

--- test.nim
+++ test.nim
@@ -1,22 +1,43 @@
 import objects, querying
 
+var world = createWorld()
+var root = blankObject()
+world.add(root)
+root.setPropR("name", "root")
+root.setPropR("aliases", @[])
+root.setPropR("location", root)
+root.setPropR("rootprop", "yes")
+
 proc testInheritance =
-  var world = createWorld()
-  var root = blankObject()
-  world.add(root)
-  root.setPropR("name", "root")
-  root.setPropR("aliases", @[])
-  root.setPropR("location", root)
-  root.setPropR("rootprop", "yes")
 
   var child = root.createChild()
+  world.add(child)
   child.setPropR("name", "child")
 
   var evenMoreChild = child.createChild()
+  world.add(evenMoreChild)
   evenMoreChild.setPropR("rootprop", "no")
 
   child.changeParent(evenMoreChild)
 
   assert(child.getPropVal("rootprop").strVal == "no")
 
+proc testQuery =
+
+  var o1 = root.createChild()
+  world.add(o1)
+
+  var o2 = root.createChild()
+  world.add(o2)
+
+  o1.moveTo(o2)
+
+  o1.setPropR("aliases", @["thingy".md])
+  assert(o2.getContents().len == 1)
+  assert(o2.query("thingy").len == 1)
+  
+
 testInheritance()
+testQuery()
+
+echo "All tests passed"
