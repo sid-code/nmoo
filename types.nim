90ead4a5166464ec89b513c2a929a4d1341e9377
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2015-02-23 18:41:09 -0700

    Add E_PERM

--- types.nim
+++ types.nim
@@ -76,7 +76,7 @@ type
       of dNil: nilVal*: int # dummy
 
   MError* = enum
-    E_NONE, E_TYPE, E_BUILTIN, E_ARGS, E_UNBOUND, E_BADCOND
+    E_NONE, E_TYPE, E_BUILTIN, E_ARGS, E_UNBOUND, E_BADCOND, E_PERM
   SymbolTable* = Table[string, MData]
   BuiltinProc* = proc(args: var seq[MData], world: var World,
                       user: MObject, symtable: SymbolTable): MData
