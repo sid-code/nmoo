53941adbe4b6e4a4795a722cd170d01989b7e364
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2015-03-15 11:43:06 -0700

    Split up main.nim
    setupmin sets up the world
    main reads it and allows the player to mess with it

---
+++ setupmin.nim
@@ -0,0 +1,71 @@
+import types, objects, querying, verbs, persist, builtins, rdstdin, strutils, os
+
+var world = createWorld("min")
+var root = blankObject()
+root.level = 0
+world.add(root)
+root.owner = root
+root.setPropR("name", "root")
+root.setPropR("aliases", @[])
+root.setPropR("rootprop", "yes")
+
+
+var genericContainer = root.createChild()
+world.add(genericContainer)
+genericContainer.setPropR("name", "generic container")
+genericContainer.setPropR("contents", @[])
+
+var nowhere = genericContainer.createChild()
+world.add(nowhere)
+
+var genericThing = root.createChild()
+
+world.add(genericThing)
+genericThing.setPropR("name", "generic thing")
+
+genericContainer.changeParent(genericThing)
+
+var genericRoom = genericContainer.createChild()
+world.add(genericRoom)
+genericRoom.setPropR("name", "generic room")
+genericRoom.setPropR("nexit", genericRoom)
+genericRoom.setPropR("eexit", genericRoom)
+genericRoom.setPropR("sexit", genericRoom)
+genericRoom.setPropR("wexit", genericRoom)
+genericRoom.setPropR("uexit", genericRoom)
+genericRoom.setPropR("dexit", genericRoom)
+
+var room = genericRoom.createChild()
+world.add(room)
+room.setPropR("name", "a room")
+
+var genericPlayer = genericThing.createChild()
+world.add(genericPlayer)
+genericPlayer.setPropR("name", "generic player")
+
+genericPlayer.isPlayer = true
+
+var player = genericPlayer.createChild()
+world.add(player)
+player.setPropR("name", "the player")
+
+discard player.moveTo(room)
+
+var eval = newVerb(
+  names =  "eval",
+  owner = root,
+
+  pubRead = true,
+  pubWrite = true,
+  pubExec = true,
+
+  doSpec = oStr,
+  prepSpec = pNone,
+  ioSpec = oNone
+)
+eval.setCode("(try (echo \"=> \" (eval dobjstr)) (echo error))")
+world.verbObj.verbs.add(eval)
+player.level = 0
+
+createDir(getWorldDir("min"))
+world.persist()
