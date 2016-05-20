e36ab6264d89c4716bcfd110b0c7ca9da1a30e10
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2016-05-20 12:24:55 -0700

    Add program to generate docs for builtins
    It's laughably simplistic but it works. I'll add a task to the nakefile
    to generate docs.

---
+++ builtindocgen.nim
@@ -0,0 +1,39 @@
+import strutils
+import nre
+import options
+import streams
+
+const
+  builtinsSourceFile = "src/builtins.nim"
+
+
+proc gendocs*(instream = newFileStream(builtinsSourceFile, fmRead),
+              outstream = newFileStream(stdout)) =
+  var currentComment = ""
+
+  while not instream.atEnd():
+    let line = instream.readLine()
+    if line.strip().len == 0:
+      continue
+
+    if line[0] == '#' and line[1] == '#':
+      if line.len == 2:
+        currentComment &= "\n"
+      else:
+        if line[2] == ' ':
+          currentComment &= line[3..^1] & "\n"
+        else:
+          currentComment &= line[2..^1] & "\n"
+    elif line[0..9] == "defBuiltin":
+      let matchOpt = line.match(re"""defBuiltin "([^"]+)":.*""")
+      if matchOpt.isSome:
+        let builtinName = matchOpt.get.captures.toSeq[0]
+        let bNameLen = builtinName.len
+        let numTildes = if bNameLen < 4: 4 else: bnameLen
+        outstream.writeLine(builtinName)
+        outStream.writeLine(repeat('~', numTildes))
+        outstream.writeLine(currentComment)
+      currentComment = ""
+
+when isMainModule:
+  gendocs()
