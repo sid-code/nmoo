b8cbbc7b5d5e9daa7cb2543d02f386006160a8d4
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2023-07-31 20:20:02 -0700

    add flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "NMOO";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    supportedSystems = ["x86_64-linux"];
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f system (import nixpkgs {inherit system;}));
+  in {
+    devShells = forAllSystems (system: pkgs: {
+      default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [nim nimlsp libxcrypt watchexec];
+      };
+    });
+  };
+}
