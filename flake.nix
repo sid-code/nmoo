4328dc8a1b31eacd96782e571274b82683eba640
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2023-08-01 21:45:08 -0700

    update flake and add envrc

--- flake.nix
+++ flake.nix
@@ -13,7 +13,22 @@
     supportedSystems = ["x86_64-linux"];
     forAllSystems = f:
       nixpkgs.lib.genAttrs supportedSystems (system:
-        f system (import nixpkgs {inherit system;}));
+        f system (import nixpkgs {
+          inherit system;
+          overlays = [
+            (self: super: {
+              nim-unwrapped = super.nim-unwrapped.overrideAttrs (o:
+                o
+                // rec {
+                  version = "2.0.0";
+                  src = super.fetchurl {
+                    url = "https://nim-lang.org/download/nim-${version}.tar.xz";
+                    hash = "sha256-vWEB2EADb7eOk6ad9s8/n9DCHNdUtpX/hKO0rdjtCvc=";
+                  };
+                });
+            })
+          ];
+        }));
   in {
     devShells = forAllSystems (system: pkgs: {
       default = pkgs.mkShell {
