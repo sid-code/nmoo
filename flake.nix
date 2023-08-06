ee91fbb43d7f98603a280eca09875e54865791a4
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2023-08-05 22:17:36 -0700

    add nim2

--- flake.nix
+++ flake.nix
@@ -3,17 +3,25 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nim2-nixpkgs.url = "github:ehmry/nixpkgs/nim";
   };
 
   outputs = {
     self,
     nixpkgs,
+    nim2-nixpkgs,
     ...
   }: let
     supportedSystems = ["x86_64-linux"];
+    nim2Overlay = system: self: super: {
+      nim = nim2-nixpkgs.legacyPackages.${system}.nim2;
+    };
     forAllSystems = f:
       nixpkgs.lib.genAttrs supportedSystems (system:
-        f system (import nixpkgs {inherit system;}));
+        f system (import nixpkgs {
+          inherit system;
+          overlays = [(nim2Overlay system)];
+        }));
   in {
     devShells = forAllSystems (system: pkgs: {
       default = pkgs.mkShell {
